5cc4f96f265779ca6e15dfaf64823507d100f7a3
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-01-10 18:42:19 +0545

    updated cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "*",
     credentials: true,
   })
 );
