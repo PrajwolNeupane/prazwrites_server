f2be911faf872975247fdbd59ce8bfb010929ac7
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-01-11 18:26:27 +0545

    working

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
