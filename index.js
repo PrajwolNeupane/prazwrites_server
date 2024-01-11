60cd6db1ee2e1be6fb3e5b5a2653990386dba78c
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-01-11 19:25:17 +0545

    fixed

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
 );
